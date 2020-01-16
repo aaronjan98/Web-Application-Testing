8169ce0fa88f8062c225a76973c2b0b7d9723572
aaronjan <aaronjan98@gmail.com>
2020-01-16 13:42:23 -0800

    added atBat & setAtBat

--- web-application-testing/src/App.js
+++ web-application-testing/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Display from './components/Display';
 
 function App() {
+  const [atBat, setAtBat] = useState();
+
+  let hit, foul, balls, strikes = 0;
+
+
+  if(strikes >= 3 || balls >= 4){
+    balls = 0;
+    strikes = 0;
+  }
+
+  if(foul){
+    if(strikes === 2){
+      return strikes;
+    } else
+    strikes += 2;
+  }
+
+  if(hit){
+    balls = 0;
+    strikes = 0;
+  }
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
--- web-application-testing/src/components/Display.js
+++ web-application-testing/src/components/Display.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Dashboard from './Dashboard';
 
-const Display = () => {
+const Display = props => {
 
     return (
-        <>
+        <div className="Display">
             <h2>Hello from Display</h2>
             <Dashboard />
-        </>
+        </div>
     );
 }
 
